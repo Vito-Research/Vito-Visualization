9ada86cbd1b2765b9480f9b265bc0edb19ef5a46
Andreas Ink <andreasink17@gmail.com>
2022-02-08 01:48:01 -0500

    Pushing to test

--- analyze.py
+++ analyze.py
@@ -113,6 +113,15 @@ def analyze():
         col1, col2 = st.columns(2)
 
         df2 = DataFrame()
+        for i in range(len(allDates)):
+            try:
+                if allDates[i] not in df["Start_Date"]:
+                
+                    allDates.remove(allDates[i])
+                    allAlertVals.remove(allAlertVals[i])
+            except:
+                    print()
+               
         df2.insert(0, "Start_Date_Risk", allDates, True)
         df2.insert(0, "NS Alerts", allAlertVals, True)
         #df = df.remove_duplicates()
