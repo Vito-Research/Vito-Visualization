104a0bfeb751abcd7901a1cc54a4eab18f157be0
Andreas Ink <andreasink17@gmail.com>
2022-02-14 18:36:32 -0500

    Removing bugs

--- Data/combineData.py
+++ Data/combineData.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import os
 import streamlit as st
+
 def file_selector(folder_path='./COVID-19-Phase2-Wearables'):
         df = pd.DataFrame()
         folder_path = folder_path 
         filenames = os.listdir(folder_path)
         csvFiles = []
-
+        count = 0
         for file in filenames:
             try:
                
                 
                 
-                print(folder_path + "/" + file + "/" + "Orig_NonFitbit_HR.csv")
-                df = df.append(pd.read_csv(folder_path + "/" + file + "/" + "Orig_NonFitbit_HR.csv", skiprows=0))
-                df.to_csv("./all.csv")
+                currentPath = (folder_path + "/" + file + "/" + "Orig_NonFitbit_HR.csv")
+                #df = df.append(pd.read_csv(folder_path + "/" + file + "/" + "Orig_NonFitbit_HR.csv", skiprows=0))
+                # df.to_csv("./all.csv")
+                os.replace(currentPath, "./COVID-19-Phase2-Wearables" + "/Data/Orig_NonFitbit_HR" + count + ".csv")
+                count += 1
             except:
                 print()
         return df
