9e8969ff070b11963eeafe9d507ad73689922eab
senalbulumulle <senal.bulumulle@gmail.com>
2022-03-30 12:07:27 -0400

    Cleaned some code in about.py

--- main/about.py
+++ main/about.py
@@ -3,22 +3,28 @@ import streamlit as st
 def about():
     
     with st.expander("Why It Matters"):
-        st.write("""The lead developer and founder of Vito, Andreas Ink, envisions more access and understanding of health.  
-
-Andreas’s Dad and Aunt's diagnosis of Parkinson’s and a combination of other conditions, resulted in his two relatives having a weakened immune system, leading Andreas to brainstorm solutions to combat the risk of infectious diseases.  
-
-In an effort to reduce transmission of infectious diseases from the university where Andreas studies to home where his immuno-compromised Dad is, Andreas, Mohamed Elbatouty, and Muhib Sheikh created Vito.
-
-
-Vito is an app that empowers people to make more informed decisions on visiting immunocompromised individuals like Andreas’s Dad and Aunt and venturing to high-risk areas for infectious disease transmission.
-""")
+        st.write("""
+                 The lead developer and founder of Vito, Andreas Ink, envisions more access and understanding of health.  
+                 Andreas’s Dad and Aunt's diagnosis of Parkinson’s and a combination of other conditions, resulted in his 
+                 two relatives having a weakened immune system, leading Andreas to brainstorm solutions to combat the risk 
+                 of infectious diseases.  
+                 
+                 In an effort to reduce transmission of infectious diseases from the university where Andreas studies to 
+                 home where his immuno-compromised Dad is, Andreas, Mohamed Elbatouty, and Muhib Sheikh created Vito.
+                 Vito is an app that empowers people to make more informed decisions on visiting immunocompromised 
+                 individuals like Andreas’s Dad and Aunt and venturing to high-risk areas for infectious disease 
+                 transmission.
+                """)
         
     with st.expander("How it Works"):
-        st.write("""Vito and it's on-device machine learning empowers people with their vitals.  
-
-Our first feature, the ability to possibly detect the onset of infectious diseases before symptom onset is based on a Stanford study that suggests abnormally high resting heart rate (heart rate while asleep) may indicate infectious illness.  
-
-Vito takes it a step further by improving NightSignal, Stanford's model by incorporating new vitals into the machine learning model, respiratory rate and blood oxygen levels into NightSignal.
+        st.write("""
+                 Vito and it's on-device machine learning empowers people with their vitals.  Our first feature, the
+                 ability to possibly detect the onset of infectious diseases before symptom onset is based on a Stanford 
+                 study that suggests abnormally high resting heart rate (heart rate while asleep) may indicate infectious 
+                 illness.  
+                 
+                 Vito takes it a step further by improving NightSignal, Stanford's model by incorporating new vitals into 
+                 the machine learning model, respiratory rate and blood oxygen levels into NightSignal.
 """)
 
     # with st.expander("Roadmap"):
@@ -68,6 +74,7 @@ Vito takes it a step further by improving NightSignal, Stanford's model by incor
           
 
         st.write("6) If two consecutive days are in red, then an alert is sent")
+        
     with st.expander("Github Links"):
         col1, col2, col3 = st.columns(3)
         link = '[Visualization App](https://github.com/AndreasInk/Vito-Visualization)'
