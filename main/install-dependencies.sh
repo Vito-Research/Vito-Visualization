ed2ecdb6707eb718a7c5199965ef1599a619232e
senalbulumulle <senal.bulumulle@gmail.com>
2022-03-30 15:39:48 -0400

    Updated install-dependencies.sh

---
+++ main/install-dependencies.sh
@@ -0,0 +1,18 @@
+########################################################
+#		install-dependencies.sh
+#		Senal Bulumulle
+#
+#
+########################################################
+
+
+
+
+sudo -H pip install sklearn
+sudo -H pip install numpy
+sudo -H pip install matplotlib
+sudo -H pip install streamlit 
+sudo -H pip install playsound
+sudo -H pip install pandas 
+sudo -H pip install FastAPI
+sudo -H pip install pydantic
